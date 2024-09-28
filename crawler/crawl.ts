ea75e3d2c28f7a6334e6234e828c5fce9f3e3e78
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-09-28 21:23:31 +0530

    create crawler to crawl a particular youtube playlist

--- crawler/crawl.ts
+++ crawler/crawl.ts
@@ -1,15 +1,51 @@
-// You don't need to import RequestQueue anymore
-import { CheerioCrawler } from "crawlee";
+import { PlaywrightCrawler } from "crawlee";
 
-const crawler = new CheerioCrawler({
-  async requestHandler({ $, request }) {
-    const title = $("title").text();
-    console.log(`The title of "${request.url}" is: ${title}.`);
+const crawler = new PlaywrightCrawler({
+  requestHandler: async ({ page }) => {
+    // better way
+    await page.waitForSelector("ytd-playlist-video-renderer");
+
+    const videoDetails = await page.$$eval(
+      "ytd-playlist-video-renderer",
+      (videoDiv) => {
+        const videoDetail = videoDiv.map((video) => {
+          const insideVideoDiv = video.querySelector(
+            "div#content > div#container"
+          );
+          const videoElement = insideVideoDiv?.querySelector(
+            "div#meta > h3 > a#video-title"
+          );
+
+          const videoTitle = videoElement
+            ? (videoElement as HTMLElement).title
+            : "";
+
+          const videoThumbnailElement = insideVideoDiv?.querySelector(
+            "ytd-thumbnail#thumbnail > a#thumbnail > yt-image > img"
+          );
+
+          const videoThumbnail = videoThumbnailElement
+            ? (videoThumbnailElement as HTMLImageElement).src
+            : "";
+
+          const result = {
+            title: videoTitle,
+            thumbnail: videoThumbnail,
+          };
+
+          return result;
+        });
+        return videoDetail;
+      }
+    );
+    console.log(videoDetails);
   },
 });
 
 async function runCrawler() {
-  await crawler.run(["https://crawlee.dev"]);
+  await crawler.run([
+    "https://www.youtube.com/playlist?list=PLRD1Niz0lz1sfeX5UEGzkC3HJGpPOE96b",
+  ]);
 }
 
 runCrawler();
