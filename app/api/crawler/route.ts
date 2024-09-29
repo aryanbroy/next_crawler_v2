9630437e86acc55fb11bdf6c040b64a638f537a0
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-09-29 16:07:04 +0530

    create route to get playlist data from local storage

--- app/api/crawler/route.ts
+++ app/api/crawler/route.ts
@@ -1,12 +1,13 @@
-import { Dataset } from "crawlee";
 import { NextResponse } from "next/server";
+import path from "path";
+import * as fs from "fs/promises";
 
 export async function GET() {
   try {
-    const dataset = await Dataset.open("default");
-    const { items } = await dataset.getData();
-    console.log(items);
-    return NextResponse.json({ message: "Working route" }, { status: 200 });
+    const dataPath = path.join(process.cwd(), "data", "crawled-data.json");
+    const readData = await fs.readFile(dataPath, "utf-8");
+    console.log(readData);
+    return NextResponse.json(JSON.parse(readData), { status: 200 });
   } catch (error) {
     console.log(error);
     return NextResponse.json({ message: "Error in route" }, { status: 402 });
