6c09331522c7bed16867db8ae0ba58747492f882
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-09-29 16:07:47 +0530

    create route to run the crawl command in terminal and send all the fetched data in a response array

---
+++ app/api/crawler/runCrawl/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from "next/server";
+import { exec } from "child_process";
+import { promisify } from "util";
+// import path from "path";
+// import * as fs from "fs/promises";
+
+const execPromise = promisify(exec);
+
+export async function POST(request: NextRequest) {
+  try {
+    const { url } = await request.json();
+    const { stdout, stderr } = await execPromise(`tsx crawler/crawl.ts ${url}`);
+
+    if (stderr) {
+      console.log(stderr);
+      return NextResponse.json(
+        { message: "Error crawling this playlist" },
+        { status: 402 }
+      );
+    }
+
+    const startDelimiter = "---RESULT_START---";
+    const endDelimiter = "---RESULT_END---";
+    const startIndex = stdout.indexOf(startDelimiter);
+    const endIndex = stdout.indexOf(endDelimiter);
+
+    const result = stdout.slice(startIndex + startDelimiter.length, endIndex);
+    const finalResult = JSON.parse(result);
+
+    return NextResponse.json(finalResult, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "Error on runCrawler" },
+      { status: 402 }
+    );
+  }
+}
